04369d06e4d2ee064b856f99781dffce1ceff8a9
Grégoire Payen de La Garanderie <gregoire.payendelagaranderie@telecom-bretagne.eu>
2011-02-22 11:00:03 +0100

    Petit nettoyage dans la classe Asservissement

--- stm32/src/asservissement.cpp
+++ stm32/src/asservissement.cpp
@@ -73,11 +73,6 @@ int asserCount = 0;
 
 void Asservissement::update(void)
 {
-    //roues.gauche.tourne(0.3);
-    //roues.droite.tourne(0.3);
-    //return;
-
-    //PositionPlusAngle before(odometrie.positionPlusAngle);
     asserCount++;
 
     if(asserCount > 87000)
@@ -105,167 +100,49 @@ void Asservissement::update(void)
         buffer_collision |= tmp;
 
 
-        if (true); //buffer_collision << (16 - nb_echantillons_buffer_collision))
-        {
-            // Pas collision !!!
-//#ifdef CAPTEURS
-//            bool is_there_someone_in_front = capteurs.getValue(Capteurs::AvantGauche) || capteurs.getValue(Capteurs::AvantDroite) || capteurs.getValue(Capteurs::Avant);
-//#else
-//            bool is_there_someone_in_front = false;
-//#endif
-            //is_there_someone_in_front = false;
-
-            //On s’arrête si l’on a atteint la cible ou s’il y a le robot adverse devant
-            //if((distance_restante > Distance(20) && !is_there_someone_in_front && !direction) || (direction && fabs(angle_restant.getValueInRadian()) > 0.1))
-            //{
-                //if( (min(fabs(angle_restant.getValueInRadian()), fabs((angle_restant-Angle(M_PI)).getValueInRadian()))< M_PI_4) && !direction)
-                float vitesse_lineaire_a_atteindre = command->getLinearSpeed();
-                float vitesse_angulaire_a_atteindre = command->getAngularSpeed();
-                {
+        float vitesse_lineaire_a_atteindre = command->getLinearSpeed();
+        float vitesse_angulaire_a_atteindre = command->getAngularSpeed();
 #ifdef ROUES
-                    float linear_duty_component = pid_filter_distance.getFilteredValue(vitesse_lineaire_a_atteindre-/*blah*/vitesse_lineaire_atteinte);
-                    linearDutySent += linear_duty_component;
-#endif
-                }
-                //else
-                //    linearDutySent=0;
+        float linear_duty_component = pid_filter_distance.getFilteredValue(vitesse_lineaire_a_atteindre-vitesse_lineaire_atteinte);
+        linearDutySent += linear_duty_component;
 
-#ifdef ROUES
-                float angular_duty_component = pid_filter_angle.getFilteredValue(vitesse_angulaire_a_atteindre-/*didou*/vitesse_angulaire_atteinte);
+        float angular_duty_component = pid_filter_angle.getFilteredValue(vitesse_angulaire_a_atteindre-vitesse_angulaire_atteinte);
 
-                angularDutySent += angular_duty_component;
+        angularDutySent += angular_duty_component;
 
-                linearDutySent = min(max(linearDutySent, -0.40),0.40);
-                angularDutySent = min(max(angularDutySent, -0.50),0.50);
+        linearDutySent = min(max(linearDutySent, -0.40),0.40);
+        angularDutySent = min(max(angularDutySent, -0.50),0.50);
 
-                    if(toto < 1 && dbgInc<DBG_SIZE)
-                    {
+            if(toto < 1 && dbgInc<DBG_SIZE)
+            {
 
         //roueGauche[caca] = odometrie.roueCodeuseGauche->getTickValue();
         //roueDroite[caca] = odometrie.roueCodeuseDroite->getTickValue();
-                        /*vitesseLin[dbgInc] = vitesse_lineaire_atteinte;
-                        vitesseLinE[dbgInc] = vitesse_lineaire_a_atteindre;
-                        linearDuty[dbgInc] = linearDutySent;*/
+                   /*vitesseLin[dbgInc] = vitesse_lineaire_atteinte;
+                   vitesseLinE[dbgInc] = vitesse_lineaire_a_atteindre;
+                   linearDuty[dbgInc] = linearDutySent;*/
 
-                        vitesseAng[dbgInc] = vitesse_angulaire_atteinte;
-                        vitesseAngE[dbgInc] = vitesse_angulaire_a_atteindre;
-                        angularDuty[dbgInc] = angularDutySent;
+                   vitesseAng[dbgInc] = vitesse_angulaire_atteinte;
+                   vitesseAngE[dbgInc] = vitesse_angulaire_a_atteindre;
+                   angularDuty[dbgInc] = angularDutySent;
 
-                        //posx[caca] = positionPlusAngleActuelle.position.x;
-                        //posy[caca] = positionPlusAngleActuelle.position.y;
-                        //angle[caca] = positionPlusAngleActuelle.angle.getValueInRadian(); //*angle_restant.getValueInRadian();*/distance_restante; //positionPlusAngleActuelle.angle.getValueInRadian()*180/M_PI;
-                        dbgInc++;
-                    }
-                toto = (toto+1) % 1;
+                   //posx[caca] = positionPlusAngleActuelle.position.x;
+                   //posy[caca] = positionPlusAngleActuelle.position.y;
+                   //angle[caca] = positionPlusAngleActuelle.angle.getValueInRadian(); //*angle_restant.getValueInRadian();*/distance_restante; //positionPlusAngleActuelle.angle.getValueInRadian()*180/M_PI;
+                   dbgInc++;
+               }
+           toto = (toto+1) % 1;
 
 
 
-                roues.gauche.tourne(min(max(-linearDutySent+angularDutySent, -0.9),0.9));
-                roues.droite.tourne(min(max(-linearDutySent-angularDutySent, -0.9),0.9));
+           roues.gauche.tourne(min(max(-linearDutySent+angularDutySent, -0.9),0.9));
+           roues.droite.tourne(min(max(-linearDutySent-angularDutySent, -0.9),0.9));
 #endif
-            /*}
-            else
-            {
-                if(stop || is_there_someone_in_front)
-                {
-#ifdef ROUES
-                    roues.gauche.tourne(0);
-                    roues.droite.tourne(0);
-#endif
-                    vitesse_lineaire_a_atteindre = Distance(0);
-                    vitesse_angulaire_a_atteindre = Angle(0);
-                    linearDutySent = 0;
-                    angularDutySent = 0;
-                }
-
-                if(en_mouvement && !is_there_someone_in_front)
-                {
-                    en_mouvement = false;
-                    strategie->done();
-                }
-            }*/
-
-        }
-        /*else // collision
-        {
-#ifdef ROUES
-            roues.gauche.tourne(0);
-            roues.droite.tourne(0);
-#endif
-            //vitesse_lineaire_a_atteindre = Distance(0);
-            //vitesse_angulaire_a_atteindre = Angle(0);
-            linearDutySent = 0;
-            angularDutySent = 0;
-
-            //if(en_mouvement)
-            //{
-            //    en_mouvement = false;
-            //    strategie->collisionDetected();
-            //}
-        }*/
 }
 
+//Some olds function which may or not have been replaced by function of the TrapezoidalCommand
 //void Asservissement::recule(Distance distance) {
     //goTo((odometrie->getPos() - distance).position);
-//}
-
-//Distance Asservissement::getVitesseLineaireAfterTrapeziumFilter(Distance vitesse_lineaire_atteinte, Distance distance_restante, Angle angle_restant)
-//{
-//
-//    // Le pivot est la distance que l'on parcourerait si on commencait à décélérer dès maintenant jusqu'une vitesse nulle
-//    Distance pivot(vitesse_lineaire_a_atteindre*vitesse_lineaire_a_atteindre/(2*acceleration_lineaire));
-//    if( (distance_restante-pivot)<=0 &&
-//         ((fabs((angle_restant - Angle(0)).getValueInRadian())>M_PI_2 && vitesse_lineaire_atteinte<=0)
-//          || (fabs((angle_restant - Angle(0)).getValueInRadian())<=M_PI_2 && vitesse_lineaire_atteinte>=0))
-//         && stop)
-//    {
-//        //On est proche de la cible et l’on doit décélerer
-//        if(vitesse_lineaire_a_atteindre>0.)
-//        {
-//            return vitesse_lineaire_a_atteindre-Distance(min(vitesse_lineaire_a_atteindre, acceleration_lineaire));
-//        }
-//        else
-//        {
-//            return vitesse_lineaire_a_atteindre-Distance(max(vitesse_lineaire_a_atteindre, -acceleration_lineaire));
-//        }
-//
-//    }
-//    else if(fabs((angle_restant - Angle(0)).getValueInRadian())>M_PI_2)
-//    {
-//        // Sinon, on est en phase d'accélération
-//        // Si la destination est derrière nous
-//        // Alors on ajoute à la vitesse une accélération négative (jusqu'à la valeur (-vitesse_lineaire_max))
-//        // Autrement dit, si on avance dans la mauvaise direction on ralenti et si on recule dans la bonne direction alors on recule plus vite
-//        return vitesse_lineaire_a_atteindre
-//               + Distance(max(-acceleration_lineaire, (-vitesse_lineaire_max-vitesse_lineaire_a_atteindre)));
-//    }
-//    else
-//    {
-//        // Sinon, la destination est devant nous
-//        // Alors on ajoute à la vitesse une accélération positive (jusqu'à la valeur (vitesse_lineaire_max))
-//        // Autrement dit, si on recule dans la mauvaise direction on ralenti et si on avance dans la bonne direction alors on avance plus vite
-//        return vitesse_lineaire_a_atteindre
-//               + Distance(min(acceleration_lineaire, (vitesse_lineaire_max-vitesse_lineaire_a_atteindre)));
-//    }
-//}
-//
-//Angle Asservissement::getVitesseAngulaireAfterTrapeziumFilter(Angle vitesse_angulaire_atteinte, Angle angle_restant)
-//{
-//    if(fabs((angle_restant - Angle(0)).getValueInRadian())>M_PI_2)
-//    {
-//        angle_restant = angle_restant-Angle(M_PI);
-//    }
-//    Angle didou(angle_restant);
-//    Angle pivot(vitesse_angulaire_a_atteindre.getValueInRadian()*vitesse_angulaire_a_atteindre.getValueInRadian()/(2*acceleration_angulaire.getValueInRadian()));
-//
-//    if((Angle(fabs(angle_restant.getValueInRadian()))-pivot).getValueInRadian()<=0)
-//    {
-//        return vitesse_angulaire_a_atteindre-Angle(copysign(min(fabs(vitesse_angulaire_a_atteindre.getValueInRadian()), acceleration_angulaire.getValueInRadian()), vitesse_angulaire_a_atteindre.getValueInRadian()));
-//    }
-//    else
-//    {
-//        return vitesse_angulaire_a_atteindre + Angle(copysign(min(acceleration_angulaire.getValueInRadian(), vitesse_angulaire_max.getValueInRadian()-fabs(vitesse_angulaire_a_atteindre.getValueInRadian())), angle_restant.getValueInRadian()));
-//    }
 //}
 
 //void Asservissement::goTo(Position position, bool stop)
