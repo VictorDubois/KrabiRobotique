225467a7a6ca3ea0a2290305110efccc4a6454a6
Xavier CORBILLON <xavier.corbillon@telecom-bretagne.eu>
2012-02-23 00:44:02 +0100

    Developement en série entière des fonctions mathématiques de base (et oui, le micro plante sinon)

--- stm32/src/Position.cpp
+++ stm32/src/Position.cpp
@@ -59,11 +59,36 @@ bool Position::operator==(const Position &p){
     return (this->x == p.x && this->y == p.y);
 }
 
+float sqrtx(float x)
+{
+    float h=x-1;
+    return 1 + h*(0.5-h*(0.125+h*(0.0625-h*0.0390625)));
+}
+
 Distance Position::getNorme(){
-    return Distance(sqrt(x*x+y*y));
+    return Distance(sqrtx(x*x+y*y));
+}
+
+Angle atanx(float x)
+{
+    float y = x*x;
+    return x*(1-y*(0.3333333333+y*(0.2-y*0.142857143)));
+}
+
+Angle atan2x(float y,float x)
+{
+    Angle f = atanx (fabs(y/x));
+    if (y>0)
+    {
+        return (x>0 ? f : (x=0 ? M_PI_2 : (M_PI-f) ));
+    }
+    else
+    {
+        return (x>0 ? -f : (x=0 ? -M_PI_2 : -(M_PI-f) ));
+    }
 }
 
 Angle Position::getAngle()
 {
-	return atan2(y,x);
+	return atan2x(y,x);
 }
