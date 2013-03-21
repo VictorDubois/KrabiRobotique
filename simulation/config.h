59764e3553983b3752bd8c7824fc5501976432c7
em6 <em6>
2013-03-21 15:00:26 +0100

    save de fichiers du sim ancien

---
+++ simulation/config.h
@@ -0,0 +1,32 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the Boost library is available */
+#define HAVE_BOOST /**/
+
+/* Name of package */
+#define PACKAGE "paprika"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "gregoire.dlg@free.fr"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "paprika"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "paprika 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "paprika"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.1"
