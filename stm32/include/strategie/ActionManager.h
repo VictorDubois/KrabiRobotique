639d5f3864731a3a4740afabf1420870d0c67bb1
lbonnafo <luc.bonnafoux@telecom-bretagne.eu>
2014-03-14 18:56:37 +0100

    Voici le debut des classes action et actionManager

---
+++ stm32/include/strategie/ActionManager.h
@@ -0,0 +1,17 @@
+#ifndef ACTIONMANAGER_H
+#define ACTIONMANAGER_H
+
+
+class ActionManager
+{
+    public:
+        ActionManager();
+        virtual ~ActionManager();
+        void main();
+    protected:
+    private:
+    vector<Action*> actions;
+    Astar astar;
+};
+
+#endif // ACTIONMANAGER_H
