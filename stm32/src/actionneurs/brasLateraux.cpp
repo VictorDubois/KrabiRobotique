440ad7d30ff6e160d2d31d8fea7c0a46e91832ad
Cecile Bouette <johndoe@example.com>
2015-03-25 21:20:47 +0100

    renommage de braslateral en brasLateraux

--- stm32/src/actionneurs/brasLateraux.cpp
+++ stm32/src/actionneurs/brasLateraux.cpp
@@ -1,45 +1,45 @@
-#include "braslateral.h"
+#include "brasLateraux.h"
 
-BrasLateral* BrasLateral::left = 0;
-BrasLateral* BrasLateral::right = 0;
+BrasLateraux* BrasLateraux::left = 0;
+BrasLateraux* BrasLateraux::right = 0;
 
 #ifdef ROBOTHW
 
-    BrasLateral::BrasLateral(Timer* timer, unsigned char OCx, float RC0degre, float RC180degres, float angleCollapsed, float angleExpanded, float angleFront)
+    BrasLateraux::BrasLateraux(Timer* timer, unsigned char OCx, float RC0degre, float RC180degres, float angleCollapsed, float angleExpanded, float angleFront)
         : angleCollapsed(angleCollapsed), angleExpanded(angleExpanded), angleFront(angleFront)
     {
         servo = new Servo(timer, OCx, RC0degre, RC180degres);
     }
 
-    void BrasLateral::expand()
+    void BrasLateraux::expand()
     {
         servo->goToAngle(angleExpanded);
     }
 
-    void BrasLateral::collapse()
+    void BrasLateraux::collapse()
     {
         servo->goToAngle(angleCollapsed);
     }
 
-    void BrasLateral::front()
+    void BrasLateraux::front()
     {
         servo->goToAngle(angleFront);
     }
 
-    void BrasLateral::initBrasLateraux()
+    void BrasLateraux::initBrasLateraux()
     {
         #if defined(STM32F40_41xxx) || defined(STM32F10X_MD) // Krabi Jr - STM32 H405 / STM32 H103
             Timer* timer = new Timer(TIM3, PERIOD_TIMER_SERVO, PRESCALER_TIMER_SERVO, CLOCK_TIMER_SERVO);
             TIM_CtrlPWMOutputs(TIM3, ENABLE);
 
-            left = new BrasLateral(timer, 1, 0.01f, 0.11f, 87.f, 51.f, 15.f);
-            right = new BrasLateral(timer, 3, 0.01f, 0.11f, 0.f, 40.f, 77.f);
+            left = new BrasLateraux(timer, 1, 0.01f, 0.11f, 87.f, 51.f, 15.f);
+            right = new BrasLateraux(timer, 3, 0.01f, 0.11f, 0.f, 40.f, 77.f);
         #else // Krabi - STM32 H107
             Timer* timer = new Timer(TIM1, PERIOD_TIMER_SERVO, PRESCALER_TIMER_SERVO, CLOCK_TIMER_SERVO);
             TIM_CtrlPWMOutputs(TIM1, ENABLE);
 
-            right = new BrasLateral(timer, 2, 0.01f, 0.11f, 20.f, 86.f, 84.f);
-            left = new BrasLateral(timer, 3, 0.01f, 0.11f, 120.f, 36.f, 1.f);
+            right = new BrasLateraux(timer, 2, 0.01f, 0.11f, 20.f, 86.f, 84.f);
+            left = new BrasLateraux(timer, 3, 0.01f, 0.11f, 120.f, 36.f, 1.f);
 
 
         #endif
@@ -51,39 +51,39 @@ BrasLateral* BrasLateral::right = 0;
 
 #include <QDebug>
 
-    BrasLateral::BrasLateral()
+    BrasLateraux::BrasLateraux()
     {}
 
-    void BrasLateral::expand()
+    void BrasLateraux::expand()
     {
 #ifndef ROBOTHW
-    qDebug() << "Bras lateral sorti";
+    qDebug() << "Bras lateraux sorti";
 #endif
     }
 
-    void BrasLateral::collapse()
+    void BrasLateraux::collapse()
     {
 #ifndef ROBOTHW
-    qDebug() << "Bras Lateral rentré";
+    qDebug() << "Bras Lateraux rentré";
 #endif
     }
 
-    void BrasLateral::front()
+    void BrasLateraux::front()
     {
 #ifndef ROBOTHW
-    qDebug() << "Bras Lateral sorti à 180°";
+    qDebug() << "Bras Lateraux sorti à 180°";
 #endif
     }
 
-    void BrasLateral::initBrasLateraux()
+    void BrasLateraux::initBrasLateraux()
     {
-        left = new BrasLateral();
-        right = new BrasLateral();
+        left = new BrasLateraux();
+        right = new BrasLateraux();
     }
 
 #endif
 
-BrasLateral* BrasLateral::getLeft()
+BrasLateraux* BrasLateraux::getLeft()
 {
     if (left==0)
         initBrasLateraux();
@@ -91,7 +91,7 @@ BrasLateral* BrasLateral::getLeft()
     return left;
 }
 
-BrasLateral* BrasLateral::getRight()
+BrasLateraux* BrasLateraux::getRight()
 {
     if (right==0)
         initBrasLateraux();
