16668952e4f21a6429fd821a433f5acae8b0bde2
Cecile Bouette <johndoe@example.com>
2015-03-26 20:53:11 +0100

    changemet de brasTapis avec des sevos numeriques

--- stm32/src/actionneurs/brasTapis.cpp
+++ stm32/src/actionneurs/brasTapis.cpp
@@ -1,57 +1,63 @@
 #include "brasTapis.h"
 
 
-BrasTapis* BrasTapis::brasTapisLeft = 0;
-BrasTapis* BrasTapis::brasTapisRight = 0;
+BrasTapis* BrasTapis::singleton = 0;
 
 #ifdef ROBOTHW
-BrasTapis::BrasTapis(Timer* timer, unsigned char OCx, float RC0degre, float RC180degres, float angleCollapsed, float angleExpanded):BrasKrabi(timer, OCx, RC0degre, RC180degres, angleCollapsed, angleExpanded, 0.f)
-{}
-
-void BrasTapis::initBrasTapis()
+BrasTapis::BrasTapis(bool cote)
 {
-//    #if defined(STM32F40_41xxx) || defined(STM32F10X_MD) // Krabi Jr - STM32 H405 / STM32 H103
-//        Timer* timer = new Timer(TIM3, PERIOD_TIMER_SERVO, PRESCALER_TIMER_SERVO, CLOCK_TIMER_SERVO);
-//        TIM_CtrlPWMOutputs(TIM3, ENABLE);
-//        this->brasTapisLeft = new BrasTapis(timer, 1, 0.01f, 0.11f, 87.f, 51.f);
-//        this->brasTapisRight = new BrasTapis(2timer, 3, 0.01f, 0.11f, 0.f, 40.f);
-
-//    #else // Krabi - STM32 H107
-        Timer* timer = new Timer(TIM1, PERIOD_TIMER_SERVO, PRESCALER_TIMER_SERVO, CLOCK_TIMER_SERVO);
-        TIM_CtrlPWMOutputs(TIM1, ENABLE);
-        brasTapisLeft = new BrasTapis(timer, 2, 0.01f, 0.11f, 20.f, 86.f);
-        brasTapisRight = new BrasTapis(timer, 3, 0.01f, 0.11f, 120.f, 36.f);
-
- //   #endif
-    brasTapisLeft->collapse();
-    brasTapisRight->collapse();
+    if (cote){   //cote est true on cree le bras droit, sinon on cree le bras gauche
+        this->positionBrasOuvert = 0x00;
+        this->positionBrasFerme = 0x00;
+        this->positionPinceOuverte = 0x00;
+        this->positionPinceFermee = 0x00;
+        this->moteurBras = 0;
+        this->moteurPinceHaut = 0;
+        this->moteurPinceBas = 0;
+    }
+    else {
+        this->positionBrasOuvert = 0x00;
+        this->positionBrasFerme = 0x00;
+        this->positionPinceOuverte = 0x00;
+        this->positionPinceFermee = 0x00;
+        this->moteurBras = 0;
+        this->moteurPinceHaut = 0;
+        this->moteurPinceBas = 0;
+    }
+    this->fermerBras();
 }
 
-void BrasTapis::expand()
+//TODO trouver une solution : comment discerner dans le singleton que l'on veut le bras droit ou le bras gauche? (2 getSingleton?)
+
+BrasTapis* BrasTapis::getSingleton(bool cote)
 {
-    brasTapisRight->expand();
-    brasTapisLeft->expand();
+    singleton = new BrasTapis(cote);
+    return singleton;
 }
 
-void BrasTapis::collapse()
+
+void BrasTapis::ouvrirBras()
 {
-    brasTapisRight->collapse();
-    brasTapisLeft->collapse();
+    ServosNumeriques::moveTo(positionBrasOuvert, moteurBras);
 }
 
-BrasTapis *BrasTapis::getRight()
+void BrasTapis::fermerBras()
 {
-    if (brasTapisRight == 0)
-        initBrasTapis();
-    return brasTapisRight;
+    ServosNumeriques::moveTo(positionBrasFerme, moteurBras);
 }
 
-BrasTapis *BrasTapis::getLeft()
+void BrasTapis::ouvrirPince()
 {
-    if (brasTapisLeft == 0)
-        initBrasTapis();
-    return brasTapisLeft;
+    ServosNumeriques::moveTo(positionPinceOuverte, moteurPinceHaut);
+    ServosNumeriques::moveTo(positionPinceOuverte, moteurPinceBas);
 }
+
+void BrasTapis::fermerPince()
+{
+    ServosNumeriques::moveTo(positionPinceFermee, moteurPinceHaut);
+    ServosNumeriques::moveTo(positionPinceFermee, moteurPinceBas);
+}
+
 
 #else
 
@@ -60,34 +66,40 @@ BrasTapis *BrasTapis::getLeft()
     BrasTapis::BrasTapis()
     {}
 
-    void BrasTapis::expand()
+    void BrasTapis::ouvrirBras()
     {
 #ifndef ROBOTHW
     qDebug() << "Bras devant sortis";
 #endif
     }
 
-    void BrasTapis::collapse()
+    void BrasTapis::fermerBras()
     {
 #ifndef ROBOTHW
     qDebug() << "Bras devant rentrés";
 #endif
     }
 
-void BrasTapis::initBrasTapis(){}
+    void BrasTapis::ouvrirPince()
+    {
+#ifndef ROBOTHW
+    qDebug() << "Pince ouverte";
+#endif
+    }
 
-BrasTapis *BrasTapis::getRight()
+void BrasTapis::fermerPince()
+    {
+#ifndef ROBOTHW
+    qDebug() << "Pince fermee";
+#endif
+    }
+
+BrasTapis *BrasTapis::getSingleton()
 {
-    if (brasTapisRight == 0)
-        initBrasTapis();
-    return brasTapisRight;
+    if (singleton == 0)
+        singleton = new BrasTapis();
+    return singleton;
 }
 
-BrasTapis *BrasTapis::getLeft()
-{
-    if (brasTapisLeft == 0)
-        initBrasTapis();
-    return brasTapisLeft;
-}
 
 #endif
